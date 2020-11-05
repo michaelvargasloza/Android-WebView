7307ce5968e9db312981892fc38c8d6a7d7e2989
Michael Vargas Loza <michaelvargasloza@gmail.com>
2020-11-05 17:37:12 -0400

    Version 1
    Carga de archivos.

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://es-es.facebook.com/");
+    }
+}
